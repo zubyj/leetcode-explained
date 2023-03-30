522f11bd5c8f8a0979131564c38b49d7c63ccd09
Zuby Javed <zubyjaved@gmail.com>
2023-03-29 22:36:13 -0700

    use the url title to find the correct youtube solution and inject it into the page

--- content.js
+++ content.js
@@ -3,14 +3,23 @@ function injectVideo(title) {
     if (solutionsTab) {
         const existingIframe = solutionsTab.parentElement.querySelector('iframe');
         if (!existingIframe) {
-            const iframe = document.createElement("iframe");
-            iframe.src = "https://www.youtube.com/embed/KLlXCFG5TnA";
-            iframe.width = "100%";
-            iframe.height = "100%";
-            iframe.allow =
-                "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
-            iframe.allowFullscreen = true;
-            solutionsTab.parentElement.insertBefore(iframe, solutionsTab);
+            // read from storage API and find the title in the JSON data
+            chrome.storage.local.get(['leetcodeProblems'], (result) => {
+                const problems = result.leetcodeProblems.questions;
+                const problem = problems.find((problem) => problem.title === title);
+                if (problem) {
+                    const iframe = document.createElement("iframe");
+                    iframe.src = problem.embedded_url;
+                    iframe.width = "100%";
+                    iframe.height = "100%";
+                    iframe.allow =
+                        "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture";
+                    iframe.allowFullscreen = true;
+                    solutionsTab.parentElement.insertBefore(iframe, solutionsTab);
+                } else {
+                    console.log(`Unable to find problem with title ${title} in the JSON file`);
+                }
+            });
         }
     }
 }

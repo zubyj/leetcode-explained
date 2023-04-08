272c801812410885cce31a1284ca853cb3f3d653
Zuby Javed <zubyjaved@gmail.com>
2023-04-07 23:43:43 -0700

    clean up the messy code

--- src/popup/popup.js
+++ src/popup/popup.js
@@ -1,64 +1,58 @@
+// popup.js
 import {
     getChatGPTAccessToken,
     ChatGPTProvider,
 } from "../background/chatgpt/chatgpt.js";
 
 async function main() {
-    []
     try {
         const accessToken = await getChatGPTAccessToken();
         chrome.storage.local.set({ accessToken });
-
         chrome.storage.local.get(["accessToken"], async ({ accessToken }) => {
             if (accessToken) {
-                const chatGPTProvider = new ChatGPTProvider(accessToken);
-                const analyzeCodeButton = document.getElementById("analyze-button");
-                analyzeCodeButton.onclick = async () => {
-                    const codeText = await getCodeFromActiveTab();
-                    if (codeText) {
-                        chatGPTProvider.generateAnswer({
-                            prompt: `Give me the time and space complexity of the following code, if it exists, in one short sentence.  \n ${codeText}`,
-                            onEvent: (event) => {
-                                if (event.type === 'answer') {
-                                    document.getElementById('time-complexity').textContent = `${event.data.text}`;
-                                    // tell the content script to add text onto the view-lines div
-                                    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-                                        chrome.tabs.sendMessage(tabs[0].id, { type: 'addText', data: event.data.text });
-                                    }
-                                    );
-                                }
-                            },
-                        });
-                    }
-                    else {
-                        document.getElementById('time-complexity').textContent = 'Unable to retrieve code. Please navigate to a Leetcode problem page and try again.';
-                    }
-                };
+                initAnalyzeCodeButton(new ChatGPTProvider(accessToken));
             } else {
                 displayLoginMessage();
             }
         });
     } catch (error) {
-        if (error.message === 'UNAUTHORIZED') {
-            displayLoginMessage();
-        } else {
-            console.error("Error:", error);
-        }
+        handleError(error);
     }
 }
 
-document.getElementById('login-button').onclick = () => {
-    chrome.runtime.sendMessage({ type: 'OPEN_LOGIN_PAGE' });
+
+document.getElementById("login-button").onclick = () => {
+    chrome.runtime.sendMessage({ type: "OPEN_LOGIN_PAGE" });
 };
 
+function handleError(error) {
+    if (error.message === "UNAUTHORIZED") {
+        displayLoginMessage();
+    } else {
+        console.error("Error:", error);
+    }
+}
+
 function displayLoginMessage() {
-    document.getElementById('login-button').classList.remove('hidden');
+    document.getElementById("login-button").classList.remove("hidden");
+}
+
+function initAnalyzeCodeButton(chatGPTProvider) {
+    const analyzeCodeButton = document.getElementById("analyze-button");
+    analyzeCodeButton.onclick = async () => {
+        const codeText = await getCodeFromActiveTab();
+        if (codeText) {
+            processCode(chatGPTProvider, codeText);
+        } else {
+            displayUnableToRetrieveCodeMessage();
+        }
+    };
 }
 
 async function getCodeFromActiveTab() {
     return new Promise((resolve) => {
         chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-            chrome.tabs.sendMessage(tabs[0].id, { type: 'getCode' }, (response) => {
+            chrome.tabs.sendMessage(tabs[0].id, { type: "getCode" }, (response) => {
                 if (chrome.runtime.lastError) {
                     resolve(null);
                 } else {
@@ -67,6 +61,33 @@ async function getCodeFromActiveTab() {
             });
         });
     });
+}
+
+function processCode(chatGPTProvider, codeText) {
+    chatGPTProvider.generateAnswer({
+        prompt: `Give me the time and space complexity of the following code, if it exists, in one short sentence.\n ${codeText}`,
+        onEvent: (event) => {
+            if (event.type === "answer") {
+                displayTimeComplexity(event.data.text);
+                sendTextToContentScript(event.data.text);
+            }
+        },
+    });
+}
+
+function displayTimeComplexity(timeComplexity) {
+    document.getElementById("time-complexity").textContent = timeComplexity;
+}
+
+function sendTextToContentScript(text) {
+    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        chrome.tabs.sendMessage(tabs[0].id, { type: "addText", data: text });
+    });
+}
+
+function displayUnableToRetrieveCodeMessage() {
+    document.getElementById("time-complexity").textContent =
+        "Unable to retrieve code. Please navigate to a Leetcode problem page and try again.";
 }
 
 main();

e0a95a8e8beb35674083d4e5e87dbab4cb45183a
Zuby Javed <zubyjaved@gmail.com>
2023-04-07 23:52:59 -0700

    clean up inject solution video code

--- src/content-script/inject-solution-video.js
+++ src/content-script/inject-solution-video.js
@@ -1,50 +1,55 @@
+/**
+ * Creates a video container element with the provided video URL.
+ * @param {string} videoUrl - The video URL.
+ * @return {HTMLDivElement} - The video container element.
+ */
+function createVideoContainer(videoUrl) {
+    const VIDEO_ASPECT_RATIO = 56.25; // 16:9 aspect ratio
 
-// Injects the embedded youtube solution into the solutions tab of the Leetcode problem
+    const container = document.createElement('div');
+    container.classList.add('video-container');
+    container.style.position = 'relative';
+    container.style.paddingBottom = `${VIDEO_ASPECT_RATIO}%`;
+
+    const video = document.createElement('object');
+    video.classList.add('youtube-video');
+    video.type = 'text/html';
+    video.data = videoUrl;
+    video.style.position = 'absolute';
+    video.style.width = '90%';
+    video.style.height = '100%';
+
+    container.appendChild(video);
+    return container;
+}
+
+/**
+ * Injects the embedded YouTube solution into the solutions tab of the LeetCode problem.
+ * @param {string} title - The problem title.
+ */
 function injectVideo(title) {
-    // Get the solutions tab
-    const solutionsTab = document.querySelectorAll('div.w-full.flex-col.overflow-auto')[1];
-    if (!solutionsTab) {
-        return;
-    }
+    const SOLUTIONS_TAB_INDEX = 1;
 
-    // Check if the video has already been injected
+    const solutionsTab = document.querySelectorAll('div.w-full.flex-col.overflow-auto')[SOLUTIONS_TAB_INDEX];
+    if (!solutionsTab) return;
+
     const existingContainer = solutionsTab.parentElement.querySelector('div.video-container');
-    if (existingContainer) {
-        return;
-    }
+    if (existingContainer) return;
 
-    // Find the problem in the JSON file and get the embedded_url
     chrome.storage.local.get(['leetcodeProblems'], (result) => {
         const problems = result.leetcodeProblems.questions;
         const problem = problems.find((problem) => problem.title === title);
 
-        // Check if the problem has the embedded_url
         if (problem && problem.embedded_url) {
-            // Create a container element and set its styles
-            const container = document.createElement('div');
-            container.classList.add('video-container');
-            container.style.position = 'relative';
-            container.style.paddingBottom = '56.25%'; // 16:9 aspect ratio
-
-            // Create an object element and set its attributes
-            const video = document.createElement('object');
-            video.classList.add('youtube-video');
-            video.type = 'text/html';
-            video.data = problem.embedded_url;
-            video.style.position = 'absolute';
-            video.style.width = '90%';
-            video.style.height = '100%';
-
-            // Append the object element to the container element
-            container.appendChild(video);
-
-            // Insert the container element before the solutions tab
+            const container = createVideoContainer(problem.embedded_url);
             solutionsTab.parentElement.insertBefore(container, solutionsTab);
         }
     });
 }
 
-// Listen for messages from the background script
+/**
+ * Handles incoming messages from the background script.
+ */
 chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
     if (request.action === 'injectVideo') {
         const title = request.title.split('-')[0].trim();

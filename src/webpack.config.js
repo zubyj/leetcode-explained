d6c013297b8cec139d4f2e97bc1f04c347c59319
Zuby Javed <zubyjaved@gmail.com>
2023-05-31 13:49:25 -0700

    begin working on v1.5

---
+++ src/webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+
+module.exports = {
+    entry: './dist/background/chatgpt/chatgpt.js',
+    output: {
+        filename: 'bundle.js',
+        path: path.resolve(__dirname, 'dist'),
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
+    mode: 'production',
+};

605d1d2031890bdf67637c201a8ddac511729885
Zuby Javed <zubyjaved@gmail.com>
2023-06-03 00:47:05 -0700

    hide buttons when logged out of chatgpt

--- src/popup/popup.ts
+++ src/popup/popup.ts
@@ -4,6 +4,8 @@ import {
 } from '../background/chatgpt/chatgpt.js';
 
 let gptResponse: HTMLElement = document.getElementById('gpt-response')!;
+let getComplexityBtn = document.getElementById('get-complexity-btn')!;
+let fixCodeBtn = document.getElementById('fix-code-btn')!;
 let infoMessage = document.getElementById('info-message')!;
 
 function initActionButton(buttonId: string, action: string, chatGPTProvider: ChatGPTProvider): void {
@@ -16,7 +18,6 @@ function initActionButton(buttonId: string, action: string, chatGPTProvider: Cha
             displayUnableToRetrieveCodeMessage();
         }
     };
-    document.getElementById(buttonId)!.classList.remove('hidden');
 }
 
 async function main(): Promise<void> {
@@ -39,9 +40,12 @@ async function main(): Promise<void> {
             initActionButton('get-complexity-btn', 'analyze', chatGPTProvider);
             initActionButton('fix-code-btn', 'fix', chatGPTProvider);
             initCopyButton();
+            getComplexityBtn!.classList.remove('hidden');
+            fixCodeBtn!.classList.remove('hidden');
         }
         else {
             displayLoginMessage();
+
         }
         document.getElementById('open-settings-btn')!.onclick = () => {
             window.location.href = 'settings.html';
@@ -50,8 +54,6 @@ async function main(): Promise<void> {
     catch (error) {
         handleError(error as Error);
     }
-
-
 }
 
 document.getElementById('login-button')!.onclick = () => {
@@ -110,35 +112,42 @@ function processCode(
     action: string,
 ): void {
     gptResponse!.textContent = '';
+    document.getElementById('analyze-code-response')!.textContent = '';
+    let getComplexityOnClick = getComplexityBtn!.onclick;
+    getComplexityBtn!.onclick = null;
+    let fixCodeOnClick = fixCodeBtn.onclick;
+    fixCodeBtn!.onclick = null;
+
     let prompt: string = '';
     if (action === "analyze") {
         prompt = `
         What is the time and space complexity of the following code (if the code exists).\n
         ${codeText}`;
         infoMessage.textContent = 'Getting the time and space complexity using ChatGPT ...'
+        document.getElementById('response-container')!.classList.add('hidden');
     } else if (action === "fix") {
         prompt = `Fix my code for the Leetcode problem and return only the fixed code without using a code block. If no code is provided in the following text, provide one using Python.\n ${codeText}`;
         infoMessage.textContent = 'Creating the solution using ChatGPT...'
+        document.getElementById('response-container')!.classList.remove('hidden');
     }
     chatGPTProvider.generateAnswer({
         prompt: prompt,
         onEvent: (event: { type: string; data?: { text: string } }) => {
             if (event.type === 'answer' && event.data) {
                 gptResponse!.textContent += event.data.text;
-                sendTextToContentScript(event.data.text);
+                if (action === 'analyze') {
+                    document.getElementById('analyze-code-response')!.textContent = gptResponse!.textContent;
+                }
             }
             if (event.type === 'done') {
                 infoMessage!.textContent = 'Done!';
                 chrome.storage.local.set({ 'gptResponse': gptResponse!.textContent });
                 (window as any).Prism.highlightAll();
+
+                getComplexityBtn!.onclick = getComplexityOnClick;
+                fixCodeBtn!.onclick = fixCodeOnClick;
             }
         },
-    });
-}
-
-function sendTextToContentScript(text: string): void {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-        chrome.tabs.sendMessage(tabs[0].id!, { type: 'addText', data: text });
     });
 }
 

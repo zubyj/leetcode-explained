c95ee9ecc1cf831d52554f279c4662962d789e44
Zuby Javed <zubyjaved@gmail.com>
2023-06-22 19:50:21 -0700

    show score followed by company name

--- src/content-script/add-solution-video.ts
+++ src/content-script/add-solution-video.ts
@@ -67,8 +67,7 @@ function createVideoContainer(videoUrl, channelName, companies) {
     companies.forEach((company) => {
         const option = document.createElement('option');
         option.value = company.name;
-        option.textContent = `${company.score} ⭐ | 🏢 ${company.name} `;
-
+        option.textContent = `⭐ ${company.score}  ${company.name} `;
         companyDropdown.appendChild(option);
     });
 

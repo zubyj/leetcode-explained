96935aab4dbf501f7614cfea6354cf0bb4dfc355
Zuby Javed <zubyjaved@gmail.com>
2023-06-23 12:18:08 -0700

    separate logic for creating DOM elements from setting styles

--- src/content-script/add-solution-video.ts
+++ src/content-script/add-solution-video.ts
@@ -1,5 +1,4 @@
 const VIDEO_ASPECT_RATIO = 56.25; // 16:9 aspect ratio
-
 /**
  * Creates a video container element with the provided video URL.
  * @param {string} videoUrl - The video URL.
@@ -7,92 +6,107 @@ const VIDEO_ASPECT_RATIO = 56.25; // 16:9 aspect ratio
  * @param {Array} companies - The array of company problems.
  * @return {HTMLDivElement} - The video container element.
  */
-function createVideoContainer(videoUrl, channelName, companies) {
-    const container = document.createElement('div');
-    container.classList.add('video-container');
-    container.style.position = 'relative';
-    container.style.display = 'flex';
-    container.style.justifyContent = 'center';
-    container.style.paddingBottom = `${VIDEO_ASPECT_RATIO}%`;
-    container.style.marginBottom = '60px';
 
-    const controlsContainer = document.createElement('div');
-    controlsContainer.style.display = 'flex';
-    controlsContainer.style.justifyContent = 'center';
-    controlsContainer.style.alignItems = 'center';
-    controlsContainer.style.position = 'absolute';
-    controlsContainer.style.width = '100%';
-    controlsContainer.style.paddingTop = '10px';
-    controlsContainer.style.boxSizing = 'border-box';
-    controlsContainer.style.color = '#fff';
-    container.appendChild(controlsContainer);
+function createStyledElement(tagName, styles = {}) {
+    const element = document.createElement(tagName);
+    for (let [key, value] of Object.entries(styles)) {
+        element.style[key] = value;
+    }
+    return element;
+}
 
-    const prevButton = document.createElement('button');
-    prevButton.style.fontSize = '20px';
-    prevButton.textContent = '⬅️';
-    prevButton.classList.add('prev-video');
-    controlsContainer.appendChild(prevButton);
+function createButton(content, className, styles = {}) {
+    const button = createStyledElement('button', styles);
+    button.textContent = content;
+    button.classList.add(className);
+    return button;
+}
 
-    const channelElement = document.createElement('div');
-    channelElement.style.fontSize = '15px';
-    channelElement.textContent = channelName;
-    channelElement.style.textAlign = 'center';
-    channelElement.style.width = '150px';
-    controlsContainer.appendChild(channelElement);
-
-    const nextButton = document.createElement('button');
-    nextButton.style.fontSize = '20px';
-    nextButton.textContent = '➡️';
-    nextButton.classList.add('next-video');
-    controlsContainer.appendChild(nextButton);
-
-    const toggleButton = document.createElement('button');
-    toggleButton.textContent = 'Hide Video';
-    toggleButton.classList.add('toggle-video');
-    toggleButton.style.marginLeft = '40px';
-    toggleButton.style.border = '1px solid white';
-    toggleButton.style.borderRadius = '5px';
-    toggleButton.style.padding = '5px 10px';
-    controlsContainer.appendChild(toggleButton);
-
-    const companyDropdown = document.createElement('select');
-    companyDropdown.name = 'Companies';
-    // Set default text to "Companies"
-    companyDropdown.style.marginLeft = '20px';
-    companyDropdown.style.border = '1px solid white';
-    companyDropdown.style.borderRadius = '5px';
-    companyDropdown.style.padding = '5px 10px';
-    companyDropdown.style.height = '35px';
+function createSelect(name, companies, styles = {}) {
+    const select = createStyledElement('select', styles);
+    select.name = name;
 
     companies.forEach((company) => {
         const option = document.createElement('option');
         option.value = company.name;
         option.textContent = `⭐ ${company.score}  ${company.name}`;
-        companyDropdown.appendChild(option);
+        select.appendChild(option);
     });
 
-    controlsContainer.appendChild(companyDropdown);
+    return select;
+}
 
+function createControlsContainer(channelName, companies) {
+    const controlsContainer = createStyledElement('div', {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        position: 'absolute',
+        width: '100%',
+        paddingTop: '10px',
+        boxSizing: 'border-box',
+        color: '#fff'
+    });
 
+    const prevButton = createButton('⬅️', 'prev-video', { fontSize: '20px' });
+    const channelElement = createStyledElement('div', {
+        fontSize: '15px',
+        textAlign: 'center',
+        width: '150px'
+    });
+    channelElement.textContent = channelName;
+    const nextButton = createButton('➡️', 'next-video', { fontSize: '20px' });
 
-    const iframe = document.createElement('iframe');
+    const toggleButtonStyles = {
+        marginLeft: '40px',
+        border: '1px solid white',
+        borderRadius: '5px',
+        padding: '5px 10px'
+    };
+    const toggleButton = createButton('Hide Video', 'toggle-video', toggleButtonStyles);
+
+    const selectStyles = {
+        marginLeft: '20px',
+        border: '1px solid white',
+        borderRadius: '5px',
+        padding: '5px 10px',
+        height: '35px'
+    };
+    const companyDropdown = createSelect('Companies', companies, selectStyles);
+
+    controlsContainer.append(prevButton, channelElement, nextButton, toggleButton, companyDropdown);
+
+    return { controlsContainer, prevButton, nextButton, toggleButton, companyDropdown };
+}
+
+function createVideoContainer(videoUrl, channelName, companies) {
+    const container = createStyledElement('div', {
+        position: 'relative',
+        display: 'flex',
+        justifyContent: 'center',
+        paddingBottom: `${VIDEO_ASPECT_RATIO}%`,
+        marginBottom: '60px'
+    });
+    container.classList.add('video-container');
+
+    const { controlsContainer, prevButton, nextButton, toggleButton, companyDropdown } = createControlsContainer(channelName, companies);
+
+    const iframe = createStyledElement('iframe', {
+        display: 'flex',
+        justifyContent: 'center',
+        position: 'absolute',
+        top: '60px',
+        width: '95%',
+        height: '95%',
+        border: '1px solid grey'
+    });
     iframe.classList.add('youtube-video');
     iframe.src = videoUrl;
-    iframe.style.display = 'flex';
-    iframe.style.justifyContent = 'center';
-    iframe.style.position = 'absolute';
-    iframe.style.top = '60px'; // Adjust this value based on the height of your controlsContainer
-    iframe.style.width = '95%';
-    iframe.style.height = '95%';
-    iframe.style.border = '1px solid grey';
     iframe.allowFullscreen = true;
-    container.appendChild(iframe);
 
-    container.iframe = iframe;
-    container.channelElement = channelElement;
-    container.companyDropdown = companyDropdown;
+    container.append(controlsContainer, iframe);
 
-    return container;
+    return { container, iframe, prevButton, nextButton, toggleButton, companyDropdown };
 }
 
 interface LeetCodeProblem {
@@ -122,21 +136,15 @@ function addVideo(title) {
     if (existingContainer) return;
 
     chrome.storage.local.get(['leetcodeProblems'], (result) => {
-        const problems = result.leetcodeProblems.questions;
-        const problem = problems.find((problem) => problem.title === title);
-
+        const problem = result.leetcodeProblems.questions.find((problem) => problem.title === title);
         if (problem?.videos?.length) {
             let currentVideoIndex = 0;
-            const container = createVideoContainer(
+            const { container, iframe, prevButton, nextButton, toggleButton, companyDropdown } = createVideoContainer(
                 problem.videos[currentVideoIndex].embedded_url,
                 problem.videos[currentVideoIndex].channel,
-                problem.companies
-                || []
+                problem.companies || []
             );
             solutionsTab.parentElement?.insertBefore(container, solutionsTab);
-
-            const prevButton = container.querySelector('button.prev-video');
-            const nextButton = container.querySelector('button.next-video');
 
             prevButton?.addEventListener('click', () => {
                 currentVideoIndex = (currentVideoIndex - 1 + problem.videos.length) % problem.videos.length;
@@ -144,7 +152,7 @@ function addVideo(title) {
                     container,
                     problem.videos[currentVideoIndex].embedded_url,
                     problem.videos[currentVideoIndex].channel,
-                    container.companyDropdown.value
+                    companyDropdown.value
                 );
             });
 
@@ -154,11 +162,10 @@ function addVideo(title) {
                     container,
                     problem.videos[currentVideoIndex].embedded_url,
                     problem.videos[currentVideoIndex].channel,
-                    container.companyDropdown.value
+                    companyDropdown.value
                 );
             });
 
-            const toggleButton = container.querySelector('button.toggle-video');
             toggleButton?.addEventListener('click', () => {
                 const videoContainer = document.querySelector('div.video-container');
                 if (videoContainer) {
@@ -182,9 +189,13 @@ function addVideo(title) {
 }
 
 function updateVideo(container, videoUrl, channelName, selectedCompany) {
-    container.iframe.src = videoUrl;
-    container.channelElement.textContent = channelName;
-    container.companyDropdown.value = selectedCompany;
+    const iframe = container.querySelector('iframe.youtube-video');
+    const channelElement = container.querySelector('div.channel');
+    const companyDropdown = container.querySelector('select.Companies');
+
+    if (iframe) iframe.src = videoUrl;
+    if (channelElement) channelElement.textContent = channelName;
+    if (companyDropdown) companyDropdown.value = selectedCompany;
 }
 
 /**

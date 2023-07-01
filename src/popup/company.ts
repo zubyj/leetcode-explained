0dae879a4445522f8f1bd9fcede9cea7205a0644
Zuby Javed <zubyjaved@gmail.com>
2023-06-30 18:59:27 -0700

    add ability to sort by id, title, and score

--- src/popup/company.ts
+++ src/popup/company.ts
@@ -1,7 +1,9 @@
+// define solutions and companyName outside of the functions so they can be accessed globally
+let solutions = [];
+let companyName = "Amazon";
+
 function main() {
 
-    let companyName = "Amazon";
-    let solutions = [];
 
     chrome.storage.local.get("clickedCompany", function (data) {
         companyName = data.clickedCompany;
@@ -9,6 +11,16 @@ function main() {
         document.title = companyName + "'s favorite problems"
     });
 
+    addCompanyProblems("Score");
+
+    // attach click listeners to table headers for sorting
+    document.getElementById('#').addEventListener('click', () => sortBy('#'));
+    document.getElementById('Title').addEventListener('click', () => sortBy('Title'));
+    document.getElementById('Score').addEventListener('click', () => sortBy('Score'));
+}
+
+// Adds the company problems by sorting method
+function addCompanyProblems(sortMethod) {
     chrome.storage.local.get("leetcodeProblems", function (data) {
         data.leetcodeProblems.questions.forEach(question => {
             if (!question.companies) return;
@@ -26,7 +38,9 @@ function main() {
 
         const table = document.getElementById("solutionTable");
 
-        solutions.sort((a, b) => b.score - a.score);
+        if (sortMethod === "Score") {
+            solutions.sort((a, b) => b.score - a.score);
+        }
 
         solutions.forEach(solution => {
             const row = table.insertRow(-1);
@@ -35,6 +49,35 @@ function main() {
             titleCell.innerHTML = `<a href="${solution.url}" target="_blank">${solution.title}</a>`;
             row.insertCell(2).innerText = solution.score;
         });
+    });
+}
+
+function sortBy(column) {
+    if (column === 'Score') {
+        solutions.sort((a, b) => b.score - a.score);
+    }
+    else if (column === 'Title') {
+        solutions.sort((a, b) => a.title.localeCompare(b.title));
+    }
+    else {
+        solutions.sort((a, b) => a.id - b.id);
+    }
+
+    // after sorting, you might want to re-render your table
+    const table = document.getElementById("solutionTable");
+
+    // remove all existing rows
+    while (table.rows.length > 1) {
+        table.deleteRow(1);
+    }
+
+    // add sorted rows
+    solutions.forEach(solution => {
+        const row = table.insertRow(-1);
+        row.insertCell(0).innerText = solution.id;
+        const titleCell = row.insertCell(1);
+        titleCell.innerHTML = `<a href="${solution.url}" target="_blank">${solution.title}</a>`;
+        row.insertCell(2).innerText = solution.score;
     });
 }
 

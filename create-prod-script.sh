76a671df22b1d3c27085903c31da0cda5fd4df0c
Zuby Javed <zubyjaved@gmail.com>
2023-07-10 17:56:21 -0700

    instead of removing all files, create zip file with relevant files

--- create-prod-script.sh
+++ create-prod-script.sh
@@ -3,43 +3,24 @@
 # Ensure we are in the project root
 cd "$(dirname "$0")"
 
+# Create necessary directories in build
+mkdir -p prod prod/node_modules prod/src/assets/images prod/src/popup
 
-# Removes everything in node_modules except for eventsource-parser and lodash-es
-#
-#
-# Create a temporary directory
-mkdir temp_node_modules
+# Copy necessary files to the build directory
+cp -r dist/* prod
+cp -r node_modules/eventsource-parser/ prod/node_modules/eventsource-parser 
+cp -r node_modules/lodash-es/ prod/node_modules/lodash-es
+cp manifest.json prod
+cp src/assets prod  # copy all files under assets
+cp -r src prod  # copy all files under popup
 
-# Copy necessary files to the temporary directory
-cp -r node_modules/eventsource-parser/dist temp_node_modules/
-cp -r node_modules/lodash-es temp_node_modules/
+# Find and copy all .html, .css and .js files from src directory to build
+find src -type f \( -iname \*.html -o -iname \*.css -o -iname \*.js \) -exec cp --parents \{\} build/ \;
 
-# Remove the node_modules directory
-rm -rf node_modules
+# Remove all .ts files from build directory
+find prod/src -type f -name "*.ts" -exec rm -f {} \;
 
-# Rename the temporary directory to node_modules
-mv temp_node_modules node_modules
-
-
-
-
-# Remove dev files not used in production
-rm -rf .git .gitignore docs build src/background src/content-script \
-src/popup/settings src/popup/popup.ts src/popup/settings.ts images/ \
-LICENSE.txt README.md 
-
-# Remove images not used in production
-rm -rf src/assets/images/screenshots src/assets/images/marquee-promo-tile.png src/assets/images/small-promo-tile.png 
-
-# Remove package, tsconfig, webpack files
-rm -rf package.json package-lock.json tsconfig.json webpack.config.js src/webpack.config.js dist/webpack.config.js
-
-# Remove all .ts files from src directory
-find src -type f -name "*.ts" -exec rm -f {} \;
-
-# Remove this file
-rm -rf create-prod-script.sh
-
-# Zip the project directory into leetcode-explained.zip
-zip -r leetcode-explained.zip .
-
+# Zip the build directory into leetcode-explained.zip
+cd prod
+zip -r ../leetcode-explained.zip .
+cd ..
